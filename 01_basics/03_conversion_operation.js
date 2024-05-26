061f932cc682cea73c4f10d50b863185bba68abf
rahulv884 <48768350+rahulv884@users.noreply.github.com>
2024-05-26 09:52:45 +0000

    conversion

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,17 @@
+let valueInNumber = "3fg3";
+
+console.log(Number(valueInNumber), 'valueInNumber');
+
+//"33" => 33
+// "3qwer3"=> NaN
+//true => 1
+//false => 0
+
+let isLoggedIn =  345
+
+console.log(Boolean(isLoggedIn));
+
+//1 => true
+//0 => false
+// "" => false
+// "Rahul" => true
