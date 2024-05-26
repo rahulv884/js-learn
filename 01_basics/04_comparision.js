395c5c5a3b87b33ce77a1883ae87455ba4d57612
rahulv884 <48768350+rahulv884@users.noreply.github.com>
2024-05-26 12:00:55 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,5 @@
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+// the reason is that == and < > >= work diffrently comparision operator consider null is 0 thats why console.log(null == 0); is false and console.log(null >= 0); is true
