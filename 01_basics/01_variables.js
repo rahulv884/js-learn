5569ec4fe8557448d91c19af63a8fc8d8b9010c8
rahulv884 <48768350+rahulv884@users.noreply.github.com>
2024-05-26 09:19:01 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 12345;
+let accountEmail = "rahul@gmail.com";
+var accountPassword = "12345";
+accountCity = "Indore";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all js code is newer version
+//alert(2+2); // we are using node js not browser
+
+//null and undefined => undfined when we declear varible but not assigned value in it and null is standalone value hai we assign a value a null than means varibale value is empty
+
+//symbol => unique
+
+console.log(typeof undefined); // output undefined
+console.log(typeof null); // output object
